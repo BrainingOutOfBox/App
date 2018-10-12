f4f62ea280536f87533dd190c1871308dfb2ff03
Oli Dias <oli.dias94@protonmail.com>
2018-10-12 08:08:56 +0200

    Initial commit

---
+++ Method635.App.Forms/Method635.App.Forms/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Method635.App.Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=99ec95ab-f8c7-42ab-91b4-46af3874744f;" +
+                     "uwp={Your UWP App secret here};" +
+                     "ios={Your iOS App secret here}",
+                     typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
