712e169a9ff9d5ebb1024fe82daf25d111840c07
Oli Dias <oli.dias94@protonmail.com>
2018-11-01 14:39:35 +0100

    Dynamic items in listview working

--- Method635.App.Forms/Method635.App.Forms/ViewModels/Navigation/MasterPageViewModel.cs
+++ Method635.App.Forms/Method635.App.Forms/ViewModels/Navigation/MasterPageViewModel.cs
@@ -1,29 +1,35 @@
 ﻿using Method635.App.Forms.ViewModels.Navigation;
-using Method635.App.Forms.Views.Navigation;
 using Prism.Mvvm;
 using System.Collections.Generic;
-using Xamarin.Forms;
 
 namespace Method635.App.Forms.ViewModels
 {
     public class MasterPageViewModel : BindableBase
     {
-        public ListView ProblemsListView { get { return _problemsListView; } }
-
-        ListView _problemsListView;
 
         public MasterPageViewModel()
         {
-            _problemsListView = new ListView();
-            var findingsList = new List<BrainstormingFindingItem>()
+            var findingsList = new List<BrainstormingFindingListItem>
             {
-                new BrainstormingFindingItem(){
-                    BindingContext = new BrainstormingFindingItemViewModel(null)
-                    {
-                        Title= "TEST BSFINDING"}
-                    }
+                new BrainstormingFindingListItem(new Models.BrainstormingFinding()
+                {
+                    Name = "TESTFINDING"
+                })
             };
-            _problemsListView.ItemsSource = findingsList;
+            FindingList = findingsList;
         }
-	}
+
+        private List<BrainstormingFindingListItem> _findingList;
+        public List<BrainstormingFindingListItem> FindingList
+        {
+            get
+            {
+                return _findingList;
+            }
+            set
+            {
+                SetProperty(ref _findingList, value);
+            }
+        }
+    }
 }
