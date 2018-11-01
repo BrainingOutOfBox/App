408c5ee3317d5d21b15401b6c2b8408fe34b9f95
Oli Dias <oli.dias94@protonmail.com>
2018-11-01 14:40:22 +0100

    Add listitem file

---
+++ Method635.App.Forms/Method635.App.Forms/ViewModels/Navigation/BrainstormingFindingListItem.cs
@@ -0,0 +1,19 @@
+﻿using Method635.App.Forms.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method635.App.Forms.ViewModels.Navigation
+{
+    public class BrainstormingFindingListItem
+    {
+        private BrainstormingFinding _finding;
+        public BrainstormingFindingListItem(BrainstormingFinding finding)
+        {
+            this._finding = finding;
+        }
+
+        public string Title { get => _finding.Name; }
+
+    }
+}
