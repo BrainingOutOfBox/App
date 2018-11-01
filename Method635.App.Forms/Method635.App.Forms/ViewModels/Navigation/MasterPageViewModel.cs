59ec2d76c4a476b020100af0681e1c9f55daf2ed
Oli Dias <oli.dias94@protonmail.com>
2018-11-01 16:04:09 +0100

    Add command for findingselection

--- Method635.App.Forms/Method635.App.Forms/ViewModels/Navigation/MasterPageViewModel.cs
+++ Method635.App.Forms/Method635.App.Forms/ViewModels/Navigation/MasterPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Method635.App.Forms.RestAccess;
 using Method635.App.Forms.ViewModels.Navigation;
+using Prism.Commands;
 using Prism.Mvvm;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +14,21 @@ namespace Method635.App.Forms.ViewModels
         {
             List<BrainstormingFindingListItem> findingsList = FillFindingListItems();
             FindingList = findingsList;
+            this.SelectFindingCommand = new DelegateCommand(SelectFinding);
         }
 
         private List<BrainstormingFindingListItem> FillFindingListItems()
         {
             var findingItems = new BrainstormingFindingRestResolver().GetAllFindingsForTeam();
+            // Encapsulate findings from model into findings to be displayed in listview
             return findingItems.Select(finding => new BrainstormingFindingListItem(finding)).ToList();
+        }
+
+        public DelegateCommand SelectFindingCommand { get; set; }
+        
+        private void SelectFinding()
+        {
+
         }
 
         private List<BrainstormingFindingListItem> _findingList;
