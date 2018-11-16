13127f0fa147437913ced226d6d8d88e4b60037e
Oli Dias <oli.dias94@protonmail.com>
2018-11-16 10:23:37 +0100

    Add context menu item

--- Method635.App.Forms/Method635.App.Forms/ViewModels/Team/TeamPageViewModel.cs
+++ Method635.App.Forms/Method635.App.Forms/ViewModels/Team/TeamPageViewModel.cs
@@ -3,6 +3,7 @@ using Method635.App.Forms.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using System;
 using System.Collections.Generic;
 
 namespace Method635.App.Forms.ViewModels.Team
@@ -23,6 +24,12 @@ namespace Method635.App.Forms.ViewModels.Team
             this.SelectTeamCommand = new DelegateCommand(SelectTeam);
             this.CreateTeamCommand = new DelegateCommand(CreateTeam);
             this.JoinTeamCommand = new DelegateCommand(JoinTeam);
+            this.LeaveTeamCommand = new DelegateCommand<BrainstormingTeam>(LeaveTeam);
+        }
+
+        private void LeaveTeam(BrainstormingTeam team)
+        {
+            Console.WriteLine("Leaving team...");
         }
 
         private void JoinTeam()
@@ -58,7 +65,15 @@ namespace Method635.App.Forms.ViewModels.Team
         public DelegateCommand SelectTeamCommand { get; }
         public DelegateCommand CreateTeamCommand { get; }
         public DelegateCommand JoinTeamCommand { get; }
-        public BrainstormingTeam SelectedTeam { get; set; }
+        public DelegateCommand<BrainstormingTeam> LeaveTeamCommand { get; }
+        private BrainstormingTeam _selectedTeam;
+        public BrainstormingTeam SelectedTeam {
+            get =>_selectedTeam;
+            set
+            {
+                SetProperty(ref _selectedTeam, value);
+            }
+        }
         public string Title => "My Teams";
 	}
 }
