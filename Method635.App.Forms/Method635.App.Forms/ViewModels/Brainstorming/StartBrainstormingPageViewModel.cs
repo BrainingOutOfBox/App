b7a8d64f7b0fde72e28e817adf4e088ea4b37368
Oli Dias <oli.dias94@protonmail.com>
2018-11-22 13:40:51 +0100

    Use absolute navigation after start brainstorming

--- Method635.App.Forms/Method635.App.Forms/ViewModels/Brainstorming/StartBrainstormingPageViewModel.cs
+++ Method635.App.Forms/Method635.App.Forms/ViewModels/Brainstorming/StartBrainstormingPageViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Method635.App.Forms.Context;
+using Method635.App.Forms.PrismEvents;
 using Method635.App.Forms.RestAccess;
 using Method635.App.Forms.RestAccess.RestExceptions;
 using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -11,20 +13,22 @@ namespace Method635.App.Forms.ViewModels.Brainstorming
     public class StartBrainstormingPageViewModel : BindableBase
 	{
         private readonly INavigationService _navigationService;
+        private readonly IEventAggregator _eventAggregator;
         private readonly BrainstormingContext _context;
 
-        public StartBrainstormingPageViewModel(INavigationService navigationService, BrainstormingContext context)
+        public StartBrainstormingPageViewModel(INavigationService navigationService, IEventAggregator eventAggregator, BrainstormingContext context)
         {
             this._navigationService = navigationService;
+            this._eventAggregator = eventAggregator;
             this._context = context;
             TapCommand = new DelegateCommand(StartBrainstorming);
         }
 
         public DelegateCommand TapCommand { get; set; }
-        private async void StartBrainstorming()
+        private void StartBrainstorming()
         {
             new BrainstormingFindingRestResolver().StartBrainstormingFinding(this._context.CurrentFinding.Id);
-            await this._navigationService.NavigateAsync("BrainstormingPage");
+            this._eventAggregator.GetEvent<RenderBrainstormingEvent>().Publish();
         }
 
         private string _connectionErrorText;
